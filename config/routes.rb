e77413ade1e2ec2edb282a9e5e91fa5ddb799754
marcotsui2003 <marcotsui2003@yahoo.com>
2016-03-16 16:18:38 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
     resources :songs, only: [:new,:index, :show, :edit]
   end
   resources :songs
+
+   scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
+  
 end
